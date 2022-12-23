b3f1048dd39c042cf9924a3ac1edc78212bdab04
Adam Major <majoradam@turr.hu>
2022-12-23 11:31:12 +0100

    viewchanger module added

---
+++ src/spal/service/viewChanger.ts
@@ -0,0 +1,24 @@
+import { ViewElement } from "../class/ViewElement.js";
+import { Display } from "../common/display.js";
+import { Scanner } from "../common/scanner.js";
+
+export module ViewChanger {
+    export function addAll(viewChangers: NodeListOf<Element>, viewElements: ViewElement[]) {
+        Array.from(viewChangers).forEach(viewChanger => {
+            document.querySelector(`#${viewChanger.id}`)?.addEventListener('click', function handleClick(event) {
+    
+                let viewName: String = viewChanger.id.substring(viewChanger.id.lastIndexOf('-') + 1);
+    
+                let viewElement: ViewElement = viewElements.find(viewElement => viewElement.name === viewName)!
+                
+                Display.view(viewElement);
+                
+                let viewChangers: NodeListOf<Element> = Scanner.viewChangerScan();
+               
+                addAll(viewChangers, viewElements);
+            })
+        })
+    }
+}
+
+
