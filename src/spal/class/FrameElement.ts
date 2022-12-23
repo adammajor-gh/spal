dcd949a72dc0211be1ba3419ca4cd22386de8832
Adam Major <majoradam@turr.hu>
2022-12-23 11:24:23 +0100

    frame element class added

--- src/spal/class/FrameElement.ts
+++ src/spal/class/FrameElement.ts
@@ -1,13 +1,23 @@
+import { FileReader } from "../common/fileReader.js";
 import { LayoutElement } from "../interface/LayoutElement.js";
 
 export class FrameElement implements LayoutElement {
     name: string;
     url: string;
     content: string;
+    build!: Function;
 
     constructor (name: string, url: string, content: string) {
         this.name = name;
         this.url = url;
         this.content = content;
+    }
+
+    static build(name: string): Promise<FrameElement> {
+        return new Promise (async (resolve, reject) => {
+            const url = `./frame/${name}/${name}.html`;
+            const content = await FileReader.read(url) as string;
+            resolve(new FrameElement(name, url, content));
+        })
     }
 }
