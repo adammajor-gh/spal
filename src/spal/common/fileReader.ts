5db13541dbb12bdcfec50a80891fdbfc9f732efe
Adam Major <majoradam@turr.hu>
2022-12-23 11:28:35 +0100

    filereader module added

--- src/spal/common/fileReader.ts
+++ src/spal/common/fileReader.ts
@@ -1,22 +1,34 @@
-//TODO: Any return type is very bad practice!!!!
+import { logger } from "./logger.js";
+import { FileContent } from "../type/FileContent.js";
 
-export function readFile(url: string): Promise<any> {
-  const extension: string = url.substring(url.lastIndexOf('.') + 1);
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.statusText)
-      }
-
-      switch(extension) {
-
-        case 'json' : 
-          return response.json() 
-          break;
-
-        case 'html' : 
-          return response.text();
-          break;
-      }
+export module FileReader {
+  export function read(url: string): Promise<FileContent> {
+    return new Promise((resolve, reject) => {
+      const extension: string = url.substring(url.lastIndexOf('.') + 1);
+      return fetch(url)
+        .then(response => {
+  
+          if (!response.ok) {
+            logger.log('error',`file not found: "${url}"`);
+          }
+    
+          switch(extension) {
+            case 'json' :
+              logger.log('debug', `file readed from: ${url}`);
+              resolve(response.json() as object); 
+              break;
+    
+            case 'html' :
+              logger.log('debug', `file readed from: ${url}`) ;
+              resolve(response.text() as unknown as string);
+              break;
+  
+            default :
+              logger.log('error', `not supported file ${url}`);
+              break;
+          }
+        })
     })
+  }
 }
+
