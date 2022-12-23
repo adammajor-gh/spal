30dc93bd85b5891a89e2b2b8de847ce318999c53
Adam Major <majoradam@turr.hu>
2022-12-23 11:28:45 +0100

    logger module added

---
+++ src/spal/common/logger.ts
@@ -0,0 +1,24 @@
+export module logger {
+    export let isSuppressWarning: boolean = false;
+    export let isDebugMode: boolean = false;
+    export function log(level: string, msg: string) {
+        switch(level) {
+            case 'info':
+                break;
+            
+            case 'debug':
+                if (isDebugMode) {
+                    console.debug(`DEBUG (${new Date(Date.now())}): ${msg}`);
+                }
+                break;
+            
+            case 'warning':
+                console.warn(`WARNING (${new Date(Date.now())}): ${msg}`);
+                break;
+
+            case 'error':
+                console.error(`ERROR (${new Date(Date.now())}): ${msg}`);
+                break;
+        }
+    }
+}
