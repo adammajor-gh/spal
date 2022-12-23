b15314ffff092419225cea0309f5060c488f50eb
Adam Major <majoradam@turr.hu>
2022-12-23 11:30:34 +0100

    create frame element added

--- src/spal/service/frame.ts
+++ src/spal/service/frame.ts
@@ -1,17 +1,23 @@
-//Class imports
+import { Config, FrameElementConfig } from '../type/Config.js';
 import { FrameElement } from '../class/FrameElement.js';
+import { logger } from '../common/logger.js';
 
-//Function imports
-import { readFile } from '../common/fileReader.js'
-
-export function readAll(frameConfig: {name: string}[]): FrameElement[]{
-    let frameElements: FrameElement[] = [];
-    frameConfig.forEach(async (frameElementConfig: {name: string}) => {
-        frameElements.push(new FrameElement(
-            frameElementConfig.name, 
-            `./frame/${frameElementConfig.name}/${frameElementConfig.name}.html`, 
-            await readFile(`./frame/${frameElementConfig.name}/${frameElementConfig.name}.html`)
-            ));
-    })
-    return frameElements;
+export module Frame {
+    function createFrameElement(frameElementConfig: FrameElementConfig): Promise<FrameElement> {
+        return new Promise (async (resolve, reject) => {
+            const frameElement: FrameElement = await FrameElement.build(frameElementConfig.name);
+            logger.log('debug', `${frameElement.name} frame element created`);
+            resolve(frameElement);
+        })
+    }
+    
+    export function readAll(config: Config): Promise<FrameElement[]>{
+        return new Promise((resolve, reject) => {
+            let frameElements: Promise<FrameElement>[] = [];
+            config.spalOptions.frame.forEach((frameElementConfig: FrameElementConfig) => {
+                frameElements.push(createFrameElement(frameElementConfig));
+            })
+            resolve(Promise.all(frameElements));
+        })
+    }
 }
