1677adf6442b237ec055816104051f80352758fb
Adam Major <majoradam@turr.hu>
2023-10-10 11:43:57 +0200

    log message type modified

--- src/util/Log.ts
+++ src/util/Log.ts
@@ -1,26 +1,26 @@
-import { Context } from "../enum/Context"
-import { LogLevel } from "../enum/LogLevel"
-import { Logger } from "../service/Logger"
+import { Context } from "../enum/Context.js"
+import { LogLevel } from "../enum/LogLevel.js"
+import { Logger } from "../service/Logger.js"
 
 export module Log {
 
-    export const info = (context: Context, message: string) => {
+    export const info = (context: Context, message: unknown) => {
         Logger.createNewLog(context, LogLevel.INFO, message);
     }
 
-    export const trace = (context: Context, message: string) => {
+    export const trace = (context: Context, message: unknown) => {
         Logger.createNewLog(context, LogLevel.TRACE, message);
     }
 
-    export const debug = (context: Context, message: string) => {
+    export const debug = (context: Context, message: unknown) => {
         Logger.createNewLog(context, LogLevel.DEBUG, message);
     }
 
-    export const warning = (context: Context, message: string) => {
+    export const warning = (context: Context, message: unknown) => {
         Logger.createNewLog(context, LogLevel.WARNING, message);
     }
 
-    export const error = (context: Context, message: string) => {
+    export const error = (context: Context, message: unknown) => {
         Logger.createNewLog(context, LogLevel.ERROR, message);
     }
 }
