a89d1960b1ea75e3551613500bc088d730baae51
Adam Major <majoradam@turr.hu>
2023-10-10 11:43:08 +0200

    logbuffer processing and log write out added

--- src/service/Logger.ts
+++ src/service/Logger.ts
@@ -1,8 +1,8 @@
 //import { Log } from "../class/Log";
 
-import { Log } from "../class/Log";
-import { Context } from "../enum/Context"
-import { LogLevel } from "../enum/LogLevel"
+import { Log } from "../class/Log.js";
+import { Context } from "../enum/Context.js"
+import { LogLevel } from "../enum/LogLevel.js"
 
 export module Logger {
     //let isInitialized: boolean = false;
@@ -13,16 +13,16 @@ export module Logger {
 
     }
 
-    export const createNewLog = (context: Context, level: LogLevel, message: string) => {
+    export const createNewLog = (context: Context, level: LogLevel, message: unknown) => {
         logBuffer.push(new Log(context, level, message));
-        processLogbuffer(logBuffer);
+        processLogBuffer(logBuffer);
     }
 }
 
-const processLogbuffer = (logBuffer: Log[]) => {
-    logBuffer.forEach(log => {
+const processLogBuffer = (logBuffer: Log[]) => {
+    logBuffer.forEach((log, index) => {
         writeOutLog(log);
-        logBuffer = logBuffer.filter(log => log != log);
+        logBuffer.splice(index, logBuffer.length)
     });
 }
 
@@ -30,18 +30,23 @@ const writeOutLog = (log: Log) => {
     switch (log.getLevel()) {
 
         case LogLevel.INFO:
+            console.info(`%c[${log.getContext()}] [${log.getLevel()}] (${log.getTimeStamp()}): ${log.getMessage()}`, log.getStyle());
             break;
             
         case LogLevel.TRACE:
+            console.trace(`%c[${log.getContext()}] [${log.getLevel()}] (${log.getTimeStamp()}): ${log.getMessage()}`, log.getStyle());
             break;
 
         case LogLevel.DEBUG:
+            console.debug(`%c[${log.getContext()}] [${log.getLevel()}] (${log.getTimeStamp()}): ${log.getMessage()}`, log.getStyle());
             break;
 
         case LogLevel.WARNING:
+            console.warn(`%c[${log.getContext()}] [${log.getLevel()}] (${log.getTimeStamp()}): ${log.getMessage()}`, log.getStyle());
             break;
 
         case LogLevel.ERROR:
+            console.error(`%c[${log.getContext()}] [${log.getLevel()}] (${log.getTimeStamp()}): ${log.getMessage()}`, log.getStyle());
             break;
     }
 }
