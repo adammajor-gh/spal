316f8bd6bf4ecf6fbca5826911d8ec6d096e0295
Adam Major <majoradam@turr.hu>
2023-10-10 11:43:36 +0200

    test logs added

--- src/service/Initializer.ts
+++ src/service/Initializer.ts
@@ -1,5 +1,13 @@
+import { Context } from "../enum/Context.js";
+import { Log } from "../util/Log.js";
+
 export module Initializer {
     export const run = () => {
         console.log('Hello SPAL!');
+        Log.info(Context.SPAL, 'Hello Info Log');
+        Log.trace(Context.SPAL, 'Hello Trace Log');
+        Log.debug(Context.SPAL, 'Hello Debug Log');
+        Log.warning(Context.SPAL, 'Hello Warning Log');
+        Log.error(Context.SPAL, 'Hello Error Log');
     }
 }
