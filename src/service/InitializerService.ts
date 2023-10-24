f84f9ff302470e907a4c217bea3160218a1ad4cd
Major Ádám <major.adam@evonit.hu>
2023-10-24 15:01:47 +0200

    Debug mode and suppress warning handled at logs

--- src/service/InitializerService.ts
+++ src/service/InitializerService.ts
@@ -1,4 +1,5 @@
 import { SpalConfig } from "../type/SpalConfig.js";
+import { LoggerService } from "./LoggerService.js";
 import { SpalConfigService } from "./SpalConfigService.js";
 import { SpalStateService } from "./SpalStateService.js";
 
@@ -6,5 +7,6 @@ export module Initializer {
     export const run = async () => {
         const spalConfig: SpalConfig = await SpalConfigService.initialize();
         await SpalStateService.initialize(spalConfig);
+        LoggerService.initialize();
     }
 }
