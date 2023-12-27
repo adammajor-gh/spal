baaef4ec2a9cfc92796c2e253c3448e1eabe2ffc
Adam Major <majoradam@turr.hu>
2023-12-27 16:09:52 +0100

    add display method

--- src/class/ViewElement.ts
+++ src/class/ViewElement.ts
@@ -1,19 +1,21 @@
-import { Context } from "../enum/Context.js";
 import { LayoutElement } from "../interface/LayoutElement.js";
-import { Log } from "../util/Log.js";
 import { FileReader } from "../util/FileReader.js";
+import { HtmlElementUtil } from "../util/HtmlElement.js";
+import { DisplayService } from "../service/DisplayService.js";
+import { Context } from "../enum/Context.js";
+import { Log } from "../util/Log.js";
 
 export class ViewElement implements LayoutElement {
 
     private readonly name: string;
-    private readonly layoutElementTagName: string;
+    private readonly targetHtmlElement: HTMLElement;
     private readonly title: string;
     private readonly url: string;
     private readonly content: string;
 
-    constructor (name: string, layoutElementTagName: string, title: string, url: string, content: string) {
+    constructor (name: string, title: string, targetHtmlElement: HTMLElement, url: string, content: string) {
         this.name = name;
-        this.layoutElementTagName = layoutElementTagName;
+        this.targetHtmlElement = targetHtmlElement;
         this.title = title
         this.url = url;
         this.content = content;
@@ -21,9 +23,6 @@ export class ViewElement implements LayoutElement {
 
     getName(): string {
         return this.name;
-    }
-    getLayoutElementTagName(): string {
-        return this.layoutElementTagName;
     }
     getTitle(): string {
         return this.title;
@@ -35,14 +34,28 @@ export class ViewElement implements LayoutElement {
         return this.content;
     }
 
+    display() {
+        try {
+            DisplayService.displayLayoutElement(this.name, this.content, this.targetHtmlElement);
+        } catch (error) {
+            Log.error(Context.SPAL, `Error during view element displaying: ${error}`);
+        }
+    }
+
     static build(title: string, url: string): Promise<ViewElement> {
-        Log.debug(Context.SPAL, `Attemt to build a new View element from: ${url}`);
         return new Promise (async (resolve) => {
-            const name = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
-            const layoutElementTagName = 'spal-view';
-            Log.debug(Context.SPAL, `New View element name: ${name}`);
-            const content = await FileReader.readFile(url);
-            resolve(new ViewElement(name, layoutElementTagName, title, url, content));
+            Log.debug(Context.SPAL, `Attemt to create new View Element: ${title}`);
+            try {
+                const name = url.substring(url.lastIndexOf('/') + 1, url.lastIndexOf('.'));
+                const targetHtmlElementName = 'spal-view';
+                const targetHtmlElement: HTMLElement = HtmlElementUtil.getHtmlElementByName(targetHtmlElementName); 
+                const content = await FileReader.readFile(url);
+                const viewElement = new ViewElement(name, title, targetHtmlElement, url, content)
+                Log.debug(Context.SPAL, `New view element created: ${title}`);
+                resolve(viewElement);
+            } catch (error) {
+                Log.error(Context.SPAL, `Error while createing a new View Element: ${title}`);
+            }
         })
     }
 }
