771104b0c918d00df60c89e8aa835472e8580beb
Adam Major <majoradam@turr.hu>
2023-12-27 15:08:50 +0100

    tag name removed

--- src/interface/LayoutElement.ts
+++ src/interface/LayoutElement.ts
@@ -1,6 +1,5 @@
 export interface LayoutElement {
     getName(): string;
-    getLayoutElementTagName(): string;
     getUrl(): string;
     getContent(): string;
     //TODO: need another property for the build method.
