5c4b79790297fd32d4aaa6a2da2d232a27745e25
Adam Major <majoradam@turr.hu>
2024-01-27 13:46:00 +0100

    Initializer added to layout service

--- src/service/LayoutService.ts
+++ src/service/LayoutService.ts
@@ -5,7 +5,19 @@ import { Log } from "../util/Log.js";
 const layoutUrl = './layout/layout.html';
 
 export module LayoutService {
-    export const generateMainLayout = async () => {
+
+    export const initialize = () => {
+        try {
+            Log.trace(Context.SPAL, 'Attemt to initialize Layout service');
+            generateMainLayout();
+            Log.trace(Context.SPAL, 'Generate main layout service success');
+        } catch (error) {
+            Log.error(Context.SPAL, `Error during initialize the Layot service: ${error}`);
+        }
+        
+    }
+
+    const generateMainLayout = async () => {
         try {
             Log.trace(Context.SPAL, 'Attemt to generate main layout');
             const spalApp: HTMLElement = document.querySelector('spal-app') as HTMLElement;
