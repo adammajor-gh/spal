a6e575300eb0fe5a73167e6f4303feba0c290f66
adammajor-gh <adam.major314@gmail.com>
2024-02-24 20:05:03 +0100

    Dec mod analyzer added to main initializer service

--- src/service/InitializerService.ts
+++ src/service/InitializerService.ts
@@ -7,10 +7,8 @@ import { SpalConfigUtil } from "../util/SpalConfig.js";
 import { SpalStateService } from "./SpalStateService.js";
 import { SpalConfig } from "../type/SpalConfig.js";
 import { AppConfig } from "../type/AppConfig.js";
-import { DirectoryReader } from "../util/DirectoryReader.js";
-import { FrameElement } from "../class/FrameElement.js";
-import { ViewElement } from "../class/ViewElement.js";
 import { LayoutService } from "./LayoutService.js";
+import { DevModeService } from "./DevModeService.js";
 
 export module Initializer {
     export const run = async () => {
@@ -28,44 +26,7 @@ export module Initializer {
 
             await LayoutService.initialize();
 
-
-            //This is cheatsheet for test purpose
-            const frameDirectoryNames: string[] = await DirectoryReader.readDirectoryContent('./frame');
-            const viewDirectoryNames: string[] = await DirectoryReader.readDirectoryContent('./view');
-
-
-            //This is how you need to deal with multiple promises in an array
-            const generateFrames = (frameDirectoryNames: string[]) => {
-                    let frameElements: FrameElement[] = [];
-                    frameDirectoryNames.forEach(async frameDirectoryName => {
-                        const frameElement = FrameElement.build(`./frame/${frameDirectoryName}/${frameDirectoryName}.html`) as unknown as FrameElement;
-                        frameElements.push(frameElement);
-                    });
-                    return Promise.all(frameElements);
-            }
-
-            const generateViews = (viewDirectoryNames: string[]) => {
-                let viewElements: ViewElement[] = [];
-
-                viewDirectoryNames.forEach(viewDirectoryName => {
-                    const viewElement = ViewElement.build(viewDirectoryName, `./view/${viewDirectoryName}/${viewDirectoryName}.html`) as unknown as ViewElement;
-                    viewElements.push(viewElement);
-                });
-                return Promise.all(viewElements);
-            }
-            
-            const frameElements: FrameElement[] = await generateFrames(frameDirectoryNames) as FrameElement[];
-            const viewElements: ViewElement[] = await generateViews(viewDirectoryNames) as ViewElement[];
-
-            console.log(frameElements[0]);
-            console.log(viewElements[0]);
-
-            
-            //-----------------------------
-
-
-
-            
+            DevModeService.initDevModeService();
 
             Log.trace(Context.SPAL, 'SPAL initialization process success');
         } catch(error) {
