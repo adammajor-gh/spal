43a3e906e8e88a3c5dfb31be1ee72707c53be2cb
adammajor-gh <adam.major314@gmail.com>
2024-02-24 20:03:49 +0100

    Add frame and view generating

--- src/service/DevModeService.ts
+++ src/service/DevModeService.ts
@@ -1,19 +1,57 @@
+
+import { FrameElement } from "../class/FrameElement.js"
+import { ViewElement } from "../class/ViewElement.js"
+import { Context } from "../enum/Context.js"
 import { DevMode } from "../enum/Devmode.js"
+import { DirectoryReader } from "../util/DirectoryReader.js"
+import { Log } from "../util/Log.js"
 import { AppStateService } from "./AppStateService.js"
 
 export module DevModeService {
 
-    export const initDevModeService = () => {
-        if (AppStateService.appState.getAppDevMode() == DevMode.RAPID) {
-            initRapidDevMode()
+    export const initDevModeService = async () => {
+        try {
+            Log.debug(Context.SPAL, `Initializing DevMode. Detected devmode: ${AppStateService.appState.getAppDevMode()}`);
+            if (AppStateService.appState.getAppDevMode() == DevMode.RAPID) {
+                await initRapidDevMode()
+            }
+        } catch (error) {
+            Log.error(Context.SPAL, `Error durring DevMode initialization: ${error}`);
         }
     }
 
-    const initRapidDevMode = () => {
-        console.log("Rapid dev mode initialized")
+    const initRapidDevMode = async () => {
+        try {
+        const frameDirectoryNames: string[] = await DirectoryReader.readDirectoryContent('./frame');
+        AppStateService.appState.setLoadedFrameElements(await generateFrames(frameDirectoryNames) as FrameElement[]);
+        
+        const viewDirectoryNames: string[] = await DirectoryReader.readDirectoryContent('./view');
+        AppStateService.appState.setLoadedViewElements(await generateViews(viewDirectoryNames) as ViewElement[])
+        } catch (error) {
+            Log.error(Context.SPAL, `Error during Rapid dev mode initialization: ${error}`);
+        }
     }
 
-    const initAdvancedDevMode = () => {
-        //Implement Advanced dev mode
+    const generateFrames = (frameDirectoryNames: string[]) => {
+        try {
+            let frameElements: FrameElement[] = [];
+            frameDirectoryNames.forEach(async frameDirectoryName => {
+                const frameElement = FrameElement.build(`./frame/${frameDirectoryName}/${frameDirectoryName}.html`) as unknown as FrameElement;
+                frameElements.push(frameElement);
+            });
+            return Promise.all(frameElements);
+        } catch (error) {
+            Log.error(Context.SPAL, `Error during generating frames: ${error}`);
+            return undefined
+        }
+    }
+
+    const generateViews = (viewDirectoryNames: string[]) => {
+        let viewElements: ViewElement[] = [];
+        viewDirectoryNames.forEach(async viewDirectoryName => {
+            const viewElement = ViewElement.build(viewDirectoryName, `./view/${viewDirectoryName}/${viewDirectoryName}.html`) as unknown as ViewElement;
+            viewElements.push(viewElement);
+        });
+        return Promise.all(viewElements);
     }
 }
