d59075e2b7d6835074a60fe509d9ccdfada278b6
Adam Major <major.adam@evonit.hu>
2024-03-05 11:39:28 +0100

    renamed

--- src/util/ViewChanger.ts
+++
@@ -1,6 +0,0 @@
-export module ViewChanger {
-    export const scan = ():NodeList => {
-        const elements: NodeList = document.querySelectorAll('[spal-viewchanger]') as NodeList;
-        return elements
-    }
-}
