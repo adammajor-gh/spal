f2cf9af1d4e4d4ea51ebf8365e390e093a1d732d
Adam Major <major.adam@evonit.hu>
2024-03-05 11:39:08 +0100

    renamed

--- src/service/ViewchangerService.ts
+++ src/service/ViewchangerService.ts
@@ -1,3 +1,10 @@
+import { ViewChanger } from "../class/ViewChanger.js";
+import { ViewChangerReader } from "../util/ViewChangerReader.js"
+import { AppStateService } from "./AppStateService.js";
+
 export module ViewchangerService {
-    
+    export const refreshViewChangers = () => {
+        const viewChnagers: ViewChanger[] = ViewChangerReader.scan();
+        AppStateService.appState.setLoadedViewChangers(viewChnagers);
+    }
 }
